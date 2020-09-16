7f3df8d80d7127ed2d816c8d1cb6d8ecb48dd6b8
nel-maker <crowndayo888@gmail.com>
2020-09-16 08:43:12 +0100

    first commit

--- src/firebase.js
+++ src/firebase.js
@@ -1,20 +1,22 @@
 import firebase from 'firebase';
 // Your web app's Firebase configuration
 
-  var firebaseConfig = {
-    apiKey: "AIzaSyDTv6WdnCvpIdzk4FdsgFKeS69L71F6GEA",
-    authDomain: "ecycl-26768.firebaseapp.com",
-    databaseURL: "https://ecycl-26768.firebaseio.com",
-    projectId: "ecycl-26768",
-    storageBucket: "ecycl-26768.appspot.com",
-    messagingSenderId: "527002975235",
-    appId: "1:527002975235:web:f7d439076d3dd783a4a236",
-    measurementId: "G-R46CMT2FSY"
-  };
- 
+var firebaseConfig = {
+  apiKey: 'AIzaSyDTv6WdnCvpIdzk4FdsgFKeS69L71F6GEA',
+  authDomain: 'ecycl-26768.firebaseapp.com',
+  databaseURL: 'https://ecycl-26768.firebaseio.com',
+  projectId: 'ecycl-26768',
+  storageBucket: 'ecycl-26768.appspot.com',
+  messagingSenderId: '527002975235',
+  appId: '1:527002975235:web:f7d439076d3dd783a4a236',
+  measurementId: 'G-R46CMT2FSY',
+};
+
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-const db = firebaseApp.fireStore()
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
 const auth = firebase.auth();
 
-const export { db,auth }
+export { db, auth };
