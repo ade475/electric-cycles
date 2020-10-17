da348340ec0c982bc4deb9912004cb56dc178f26
Dayo Adenij <adeniji229@gmail.com>
2020-10-17 17:30:12 +0100

    revised mode

--- front/src/firebase.js
+++ front/src/firebase.js
@@ -3,13 +3,13 @@ import 'firebase/firestore';
 // Your web app's Firebase configuration
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDDF2sQs25MwEJ83hjaiOW5o1mKKyzCewo',
-  authDomain: 'electric-cycles.firebaseapp.com',
-  databaseURL: 'https://electric-cycles.firebaseio.com',
-  projectId: 'electric-cycles',
-  storageBucket: 'electric-cycles.appspot.com',
-  messagingSenderId: '598208257852',
-  appId: '1:598208257852:web:fe74ba1b0bcb1380534839',
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOM,
+  databaseURL: process.env.REACT_APP_DB_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MSG_SEND,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 // Initialize Firebase
